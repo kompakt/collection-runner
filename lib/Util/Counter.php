68fe7db92bca2829d953c4738901824ba3ceaa7f
sirprize <hoegl@backdoor.ch>
2016-11-01 12:10:47 +0100

    Add incident counter

---
+++ lib/Util/Counter.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the kompakt/collection-runner package.
+ *
+ * (c) Christian Hoegl <chrigu@sirprize.me>
+ *
+ */
+
+namespace Kompakt\CollectionRunner\Util;
+
+class Counter
+{
+    protected $stacks = array();
+
+    public function add($name, $id)
+    {
+        foreach ($this->stacks as $k => $v)
+        {
+            if (array_key_exists($id, $this->stacks[$k]))
+            {
+                // any given id may only exist once across all stacks
+                unset($this->stacks[$k][$id]);
+            }
+        }
+
+        $this->stacks[$name][$id] = 1;
+        return $this;
+    }
+
+    public function count($name)
+    {
+        if (array_key_exists($name, $this->stacks))
+        {
+            return count($this->stacks[$name]);
+        }
+
+        return 0;
+    }
+
+    public function getTotal()
+    {
+        $total = 0;
+
+        foreach ($this->stacks as $name => $id)
+        {
+            $total += count($this->stacks[$name]);
+        }
+
+        return $total;
+    }
+
+    public function getStacks()
+    {
+        $stacks = array();
+
+        foreach ($this->stacks as $name => $id)
+        {
+            $stacks[$name] = count($this->stacks[$name]);
+        }
+
+        return $stacks;
+    }
+}
